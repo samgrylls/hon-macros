347a2dfe64c0f0095eb1dc98e3a33be3023f6c4e
Sam Kenny <sjkenny1@berkeley.edu>
2020-03-15 12:47:02 -0700

    initial commit of creep stacker

--- BoundingBox.py
+++ BoundingBox.py
@@ -1,6 +1,41 @@
 class BoundingBox:
-    def __init__(self, x, y, width, height):
+    def __init__(self, coords, mode='corners'):
+        if mode == 'corners':
+            self.x = coords[0]
+            self.y = coords[1]
+            self.width = coords[2] - coords[0]
+            self.height = coords[3] - coords[1]
+        elif mode == 'relative':
+            self.x = coords[0]
+            self.y = coords[1]
+            self.width = coords[2]
+            self.height = coords[3]
+
+    def as_dict(self):
+        bb_dict_keys = ['left', 'top', 'width', 'height']
+        return dict(zip(bb_dict_keys, [self.x, self.y, self.width, self.height]))
+
+    def quadrant(self, quadrant):
+        if quadrant == 'top_left':
+            return BoundingBox([self.x, self.y, self.width//2, self.height//2], mode='relative')
+        elif quadrant == 'top_right':
+            return BoundingBox([self.x + self.width//2, self.y, self.width//2, self.height//2], mode='relative')
+        elif quadrant == 'bottom_left':
+            return BoundingBox([self.x, self.y + self.height//2, self.width//2, self.height//2], mode='relative')
+        elif quadrant == 'bottom_right':
+            return BoundingBox([self.x + self.width//2, self.y + self.height//2, self.width//2, self.height//2],
+                               mode='relative')
+        else:
+            raise ValueError('Invalid quadrant specified')
+
+
+class Point:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+
+
+class CreepCamp:
+    def __init__(self, location, run_away_coords):
+        self.location = location
+        self.run_away_coords = run_away_coords
--- HonMacroController.py
+++ HonMacroController.py
@@ -1,39 +1,61 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
-import pyautogui, sys
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import pyautogui
+import sys
+import window_functions as wf
+from config import regions_of_interest as roi
+
+from CreepStacker import DoubleCreepStack
+
 
 class HonMacroController(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
+        self.hon_windows = wf.init_hon_windows()
+        self.tasks = []
         self.initUI()
+        self.threadpool = QThreadPool()
 
     def initUI(self):
-        btn1 = QPushButton("move top left", self)
+        btn1 = QPushButton("stack easy legion creep", self)
         btn1.move(30, 50)
 
         btn2 = QPushButton("move top right", self)
-        btn2.move(150, 50)
+        btn2.move(200, 50)
 
-        btn1.clicked.connect(self.move_top_left)
+        btn3 = QPushButton("reset hon windows", self)
+        btn3.move(30, 150)
+
+        btn1.clicked.connect(self.stack_easy_legion_creep)
         btn2.clicked.connect(self.move_top_right)
+        btn3.clicked.connect(self.re_initialize_hon_windows)
 
         self.statusBar()
 
-        self.setGeometry(300, 300, 290, 150)
+        self.setGeometry(370, 300, 290, 300)
         self.setWindowTitle('Event sender')
         self.show()
 
-    @pyqtSlot()
-    def move_top_left(self):
-        pyautogui.moveTo(100, 200)
+    # need a task queue
+    # each program loop, check conditions, and execute tasks if applicable
+    def stack_easy_legion_creep(self):
+        camp_location = roi.screen_size_1600x1024['legion_easy_double_pull']
+        creepStacker = DoubleCreepStack(self.hon_windows[0], camp_location)
+        self.threadpool.start(creepStacker)
 
     @pyqtSlot()
     def move_top_right(self):
         pyautogui.moveTo(2000, 200)
+        self.tasks.append('move_top_right')
+
+    @pyqtSlot()
+    def re_initialize_hon_windows(self):
+        self.hon_windows = wf.init_hon_windows()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = HonMacroController()
     sys.exit(app.exec_())
